61bb07f6fb1d3b3bc84d503bba42fe550af236d0
fresqo <rjdesigner@hotmail.com>
2016-08-09 22:02:54 +1000

    Update Functions.hpp

--- Altis_Life.Altis/Functions.hpp
+++ Altis_Life.Altis/Functions.hpp
@@ -170,7 +170,6 @@ class Life_Client_Core {
         class clearVehicleAmmo {};
         class dropItems {};
         class escInterupt {};
-        class fadesound {};
         class fetchCfgDetails {};
         class fetchDeadGear {};
         class fetchVehInfo {};
